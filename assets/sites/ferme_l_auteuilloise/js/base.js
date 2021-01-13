24c0d7a08924601d49d30ed2472157a4827ba9e9
francois-stjacques <francois-stjacques@users.noreply.github.com>
2021-01-12 22:09:01 -0500

    Ajout de JavaScript
    Du code JavaScript a été intégré, notamment pour rendre invisible l'image et pour changer les couleurs du site

---
+++ assets/sites/ferme_l_auteuilloise/js/base.js
@@ -0,0 +1,46 @@
+function cacheImg() {
+    var cacher = document.getElementById("photo");
+    if (cacher.style.display ==="none") {
+        cacher.style.display = "block";
+    } else {
+        cacher.style.display = "none";
+    }
+}
+
+    document.getElementById("clair").addEventListener("click", function() {
+        var i;
+        var j;
+        let body = document.querySelector('body');
+        let titre = document.querySelectorAll('h3');
+        let cCouleur = document.querySelector('.carreCouleur');
+        let textuel = document.querySelectorAll('p');
+        body.style.backgroundColor = "lightgreen";
+        cCouleur.style.backgroundColor = "darkturquoise";
+
+        for (i = 0; i < titre.length; i++){
+            titre[i].style.color = "darkgoldenrod";
+        }
+
+        for (j = 0; j < textuel.length; j++){
+            textuel[j].style.backgroundColor = "burlywood";
+        }
+    });
+
+    document.getElementById("sombre").addEventListener("click", function() {
+        var k;
+        var l;
+        let body1 = document.querySelector('body');
+        let titre1 = document.querySelectorAll('h3');
+        let cCouleur1 = document.querySelector('.carreCouleur');
+        let textuel1 = document.querySelectorAll('p');
+        body1.style.backgroundColor = "black";
+        cCouleur1.style.backgroundColor = "yellow";
+
+        for (k = 0; k < titre1.length; k++){
+            titre1[k].style.color = "white";
+        }
+        
+        for (l = 0; l < textuel1.length; l++){
+            textuel1[l].style.backgroundColor = "grey";
+        }
+    });
