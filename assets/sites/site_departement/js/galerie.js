8245d0ded195ab2e753a59e4ed9ff3dfa2bbb982
francois-stjacques <francois-stjacques@users.noreply.github.com>
2021-01-27 10:53:46 -0500

    Retrait du site du département

--- assets/sites/site_departement/js/galerie.js
+++
@@ -1,9 +0,0 @@
-/*(function(){
-    // Code JS de la galerie
-    //alert(`Test galerie`);
-    let galerie = document.querySelector(".img-parent");
-    let light = document.querySelector("#img-lightb-01");
-    galerie.addEventListener("click", function() {
-        light.classList.toggle("hide");
-    })
-})();*/
